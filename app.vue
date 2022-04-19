aa71a9e88204a25a7a513ebe069f09d228ad3e6c
Cory LePage <courses@razorcx.com>
2022-04-19 14:56:13 -0700

    move link imports from app.vue to nuxt.config.ts

--- app.vue
+++ app.vue
@@ -6,6 +6,6 @@
 </template>
 
 <style scoped>
-@import "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css";
-@import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+/* @import "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css"; */
+/* @import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" */
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,20 @@ import { defineNuxtConfig } from "nuxt3";
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   ssr: false,
+  meta: {
+    meta: [
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+    ],
+    script: [],
+    link: [
+      {
+        rel: "stylesheet",
+        href: "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css",
+      },
+      {
+        rel: "stylesheet",
+        href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css",
+      },
+    ],
+  },
 });
